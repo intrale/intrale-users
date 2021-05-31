5635f6b7eabcee2e434a937fa3e4883808737b33
llarreta <leonel@larreta.com.ar>
2021-05-31 14:43:46 -0300

    Se quita obligatoriedad de atributos

--- src/main/java/ar/com/intrale/cloud/messages/ReadUserRequest.java
+++ src/main/java/ar/com/intrale/cloud/messages/ReadUserRequest.java
@@ -1,15 +1,11 @@
 package ar.com.intrale.cloud.messages;
 
-import javax.validation.constraints.NotBlank;
-
 import ar.com.intrale.cloud.Request;
-import edu.umd.cs.findbugs.annotations.NonNull;
 import io.micronaut.core.annotation.Introspected;
 
 @Introspected
 public class ReadUserRequest extends Request {
-    @NonNull
-    @NotBlank
+
 	private String email;
 	
 	public String getEmail() {
